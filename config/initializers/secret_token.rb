66652ed66c703e441b036128f71297e48f0fc2da
liang3404814 <liang3404814@gmail.com>
2013-08-10 16:22:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pengoo::Application.config.secret_token = '8306607b543a38407f47b5fd55cbb019324c05ff8af9b595fce6002fcea16864994fcf7e9a9f5d0b3dacb10ac65200b7912f5523ae8eb427f76c210af30af9fd'
